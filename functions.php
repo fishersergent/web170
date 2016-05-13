c3512474d2330996b6b89b9f45fb75dbb2108db6
fishersergent <efishe03@seattlecentral.edu>
2016-05-13 15:55:28 -0700

    Create functions.php
    Create Your Main & Sub Menu Systems

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+Theme Name: Westwood Design
+Author: Rosie Fisher-Sergent
+Author URI: http://www.fishersergent.com/
+Description: Theme for the Westwood Design landscaping company website
+Version: 1.0
+*/
+
+//Register My Menus
+register_nav_menus(array(
+    'main-menu' => __( 'Main' ),
+    ));
+
+
+
+?>
