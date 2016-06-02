c2b2054cd20c710ba8a0185eb54209bf405d085a
fishersergent <efishe03@seattlecentral.edu>
2016-06-01 19:31:15 -0700

    Add a Widget to Your Site in Your Sidebar
    Add a Widget to Your Site in Your Sidebar

--- functions.php
+++ functions.php
@@ -27,4 +27,36 @@ register_sidebars(array(
 ));
 //
 
+//get_my_title_tag function for SEO
+
+function get_my_title_tag() {
+    global $post;
+    
+    if(is_front_page()) {//if its the front page get the description/tagline
+        bloginfo('description');
+        
+    }elseif(is_page || is_single){//if its a page or single get the title
+        
+        the_title();
+    }else{//everything else, get the description/tagline
+        
+        bloginfo('description');
+    }
+    
+    if($post->post_parent){//for parent page
+        echo ' | ';
+        echo get_the_title($post->post_parent);//get the title for that page
+    }
+    
+    echo ' | '; //echo fun stuff for more google fun
+    bloginfo('name');
+    echo ' | ';
+    echo 'Seattle, WA ';  
+}
+
+
+//add page excerpt support for wordpress
+	add_post_type_support( 'page', 'excerpt' );
+
+//
 ?>
